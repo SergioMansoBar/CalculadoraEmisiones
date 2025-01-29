06c08f2262742c36051279045de7e0d9bcc23944
JuanGuar <jguaivo@etsii.upv.es>
2025-01-29 10:54:01 +0100

    CalculadoraDeEmisionesConInicio_v5.py

--- calculadoraDeEmisionesConInicio_v5.py
+++ calculadoraDeEmisionesConInicio_v5.py
@@ -4,17 +4,15 @@ import tkinter as tk
 from tkinter import ttk, PhotoImage, simpledialog, messagebox, END
 
 
-#Variables globales
+# Variables globales
 EMISIONES_TRANSPORTE = {'coche': 0.120, 'avion': 0.255, 'bus': 0.045}
 EMISIONES_ELECTRICIDAD = 0.233
 EMISIONES_RESIDUOS = 0.8
 
 # Configurar la base de datos SQLite
 base = sqlite3.connect('huella_carbono3.0.db')
-#.cursor te permite interactuar con la base de datos
 cursor = base.cursor()
 
-# .excute es para ejecutar la tabla y realizar cambios
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -25,27 +23,16 @@ CREATE TABLE IF NOT EXISTS usuarios (
     huella_total REAL NOT NULL
 )
 ''')
-#.commit se usa para guardar de forma permanente los cambios
 base.commit()
 
-'''
-Función utilizada para obtener el nombre del usuario mediante comando
-de voz (se aportará una versión sin el por si alguién no tuviera micrófono
-'''
 def obtener_nombre_por_voz():
-    #la clase regonizer es la responsable de convertir el audio en texto
     recognizer = sr.Recognizer()
-    #usa la clase microphone como fuente de entrada de audio
     with sr.Microphone() as source:
         print("Por favor, di tu nombre...")
-        #.adjust_for_ambient_noise analiz el ruido de fondo para que el sistema ignore 
-        #lo que no sea la voz principal
         recognizer.adjust_for_ambient_noise(source)
-        #.listen hace que se quede a la escucha y almacena en la variable audio
         audio = recognizer.listen(source)
 
     try:
-        #.recognizer_google convierte el audio capturado en texto y lo guarda en la variable nombre
         nombre = recognizer.recognize_google(audio, language="es-ES")
         print("Has dicho: " + nombre)
         return nombre
@@ -56,21 +43,13 @@ def obtener_nombre_por_voz():
         print("No se pudo conectar al servicio de reconocimiento de voz.")
         return None
 
-'''
-Función básica de cálculo de la huella de carbono
-'''
 def calcular_huella_carbono(transporte, electricidad, residuos):
-
     huella_transporte = sum(EMISIONES_TRANSPORTE[modo] * km for modo, km in transporte.items() if modo in EMISIONES_TRANSPORTE)
     huella_electricidad = electricidad * EMISIONES_ELECTRICIDAD
     huella_residuos = residuos * EMISIONES_RESIDUOS
 
     return huella_transporte, huella_electricidad, huella_residuos
 
-'''
-Función de asignación del nombre de usuario en función del obtenido
-por la función de obtener_nombre_por_voz()
-'''
 def obtener_datos_usuario():
     nombre_usuario = obtener_nombre_por_voz()
     if not nombre_usuario:
@@ -79,11 +58,6 @@ def obtener_datos_usuario():
 
     transporte = {}
 
-    '''
-    Función destinada al tratamiento de escepciones en cuanto a introducir
-    algún caracter que no sea numérico y a la obtención de cada valor mediante un
-    input siguiendo el método simpledialog.askstring
-    '''
     def obtener_numero(prompt):
         while True:
             entrada = simpledialog.askstring("Input", prompt)
@@ -116,11 +90,8 @@ def obtener_datos_usuario():
     registrar_usuario(nombre_usuario, huella_transporte, huella_electricidad, huella_residuos, huella_total)
     mostrar_resultados(huella_total, nombre_usuario)
 
-'''
-Función destinada a la introduccción de los resultados en la caja de texto para poder mostrarlos
-'''
 def mostrar_resultados(huella_total, nombre_usuario):
-    cajaTexto1.config(state="normal")
+    cajaTexto1.config(state="normal")  # Habilitar la caja de texto para mostrar resultados
     cajaTexto1.delete("1.0", END)
     cajaTexto1.insert(END, f"\nHola {nombre_usuario}, tu huella de carbono total al mes es de {huella_total:.2f} kg CO2 equivalentes.\n")
     cajaTexto1.insert(END, "Sugerencias para reducir tu huella de carbono:\n")
@@ -130,16 +101,11 @@ def mostrar_resultados(huella_total, nombre_usuario):
         cajaTexto1.insert(END, "- Separa mejor los residuos reciclables.\n")
     else:
         cajaTexto1.insert(END, "- ¡Buen trabajo! Sigue reduciendo tu huella.\n")
-    cajaTexto1.config(state="disabled")
+    cajaTexto1.config(state="disabled")  # Volver a deshabilitar la caja después de mostrar los resultados
 
-'''
-Función para abrir la segunda interfaz destinada al cálculo de la huella de carbono
-y cuyo comando pertenecerá al boton INICIO
-'''
 def abrir_ventana_principal():
-    global cajaTexto1, tabla #definimos cajaTexto1 como variable global para que pueda ser usada fuera
+    global cajaTexto1, tabla
     
-    #En tkinter, tk.Toplevel se utiliza para crear una nueva ventana separada de la ventana principal
     ventanaPrincipal = tk.Toplevel()
     ventanaPrincipal.geometry("925x600")
     ventanaPrincipal.title("Calculadora de emisiones")
@@ -157,17 +123,13 @@ def abrir_ventana_principal():
     frame2 = tk.Frame(ventanaPrincipal, bg="#f0fff0", padx=10, pady=10)
     frame2.grid(row=0, column=1, padx=10, pady=10, sticky="n")
     
-    #wrap="word" lo que hace es si una palabra no cabe completa al final de una línea, toda la palabra se mueve a la siguiente línea.
-    #relief="solid" coloca la apariencia del borde en este caso "solid" que nos permite que el borde tenga grosor
     cajaTexto1 = tk.Text(frame2, height=10, width=40, bg="#ffffff", fg="#006400", font=("Arial", 10), wrap="word", relief="solid", borderwidth=1)
     cajaTexto1.grid(row=0, column=0)
+    cajaTexto1.config(state="disabled")  # Establecer la caja como no editable al principio
 
     frame_tabla = tk.Frame(ventanaPrincipal, bg="#f0fff0", padx=10, pady=10)
     frame_tabla.grid(row=1, column=0, columnspan=2, pady=10)
 
-    #El Treeview es n widget de tkinter que sirve para mostrar datos en forma de tabla
-    #columns= es un argumento para especificar los nombres de las columnas
-    #.heading es una instrucción que establece a cada encabezado su respectivo nombre
     tabla = ttk.Treeview(frame_tabla, columns=("ID", "Nombre", "Transporte", "Electricidad", "Residuos", "Huella Total"), show="headings")
     tabla.heading("ID", text="ID")
     tabla.heading("Nombre", text="Nombre")
@@ -176,7 +138,6 @@ def abrir_ventana_principal():
     tabla.heading("Residuos", text="Gasto Residuos")
     tabla.heading("Huella Total", text="Huella Total")
 
-    # Este for nos permite hacer que el ancho de todas las columnas sea el mismo y que el texto esté centrado
     for col in ("ID", "Nombre", "Transporte", "Electricidad", "Residuos", "Huella Total"):
         tabla.column(col, width=150, anchor="center")
         
@@ -184,18 +145,12 @@ def abrir_ventana_principal():
     
     cargar_usuarios_en_tabla()
 
-    # Crear un frame para los botones
     frame_botones = tk.Frame(ventanaPrincipal, bg="#f0fff0", padx=10, pady=10)
     frame_botones.grid(row=2, column=0, columnspan=2, pady=10)
 
-    # Botón BORRAR debajo de la tabla
     boton_borrar = tk.Button(frame_botones, text="BORRAR", font=("Arial", 12), bg="#FF4500", fg="white", command=borrar_usuario_seleccionado)
     boton_borrar.grid(row=0, column=0, padx=10)
 
-'''
-Función que crea un insert para agragar usuarios a la base de datos a la vez de llamar a la función cargar_usuarios_en_tabla
-para poder registrar a la vez los usuarios en la tabla de la interfaz gráfica
-'''
 def registrar_usuario(nombre, huella_transporte, huella_electricidad, huella_residuos, huella_total):
     cursor.execute('''
     INSERT INTO usuarios (nombre, gasto_transporte, gasto_electricidad, gasto_residuos, huella_total)
@@ -204,23 +159,13 @@ def registrar_usuario(nombre, huella_transporte, huella_electricidad, huella_res
     base.commit()
     cargar_usuarios_en_tabla()
 
-'''
-Función encargada de cargar los datos desde la base de datos
-y mostrarlos en la interfaz en la correspondiente tabla
-'''
 def cargar_usuarios_en_tabla():
-    for item in tabla.get_children(): #obtiene todos los elementos de la tabla
-        # y devuelve una lista de todas las filas de la tabla
-        tabla.delete(item) #elimina dicha lista para actualizarla sin repetidos
-    cursor.execute('SELECT * FROM usuarios') #recupera los datos de la BD para volverlos a cargar
-    for fila in cursor.fetchall(): #.fetchall coge todos los resultados y los guarda en una tupla
-        tabla.insert("", "end", values=fila) #inserta las nuevas filas y
-        #values= filas es para que cada elemento de la tupla corresponda con una columna de la tabla de la interfaz
+    for item in tabla.get_children():
+        tabla.delete(item)
+    cursor.execute('SELECT * FROM usuarios')
+    for fila in cursor.fetchall():
+        tabla.insert("", "end", values=fila)
 
-'''
-Función encargada de darle funcionalidad al botón BORRAR para que este pueda eliminar al usuario seleccionado
-tanto del programa como de la base de datos
-'''
 def borrar_usuario_seleccionado():
     selected_item = tabla.selection()
     if not selected_item:
@@ -234,7 +179,6 @@ def borrar_usuario_seleccionado():
 
     cargar_usuarios_en_tabla()
 
-# creamos la ventana principal
 ventana = tk.Tk()
 ventana.title("Calculadora pantalla de inicio")
 ventana.geometry("800x600")
@@ -243,14 +187,12 @@ ventana.configure(bg="white")
 titulo = tk.Label(ventana, text="Calculadora de Huella de Carbono", font=("Arial", 24, "bold"), fg="#228B22", bg="white")
 titulo.pack(pady=20)
 
-# creamos la imagen y la incluimos al pack
 imagen_path = "huella.png"
 imagen = PhotoImage(file=imagen_path)
 imagen = imagen.subsample(2, 2)
 label_imagen = tk.Label(ventana, image=imagen, bg="white")
 label_imagen.pack(pady=20)
 
-# Botón de inicio que nos lleva a la calculadora
 boton_inicio = tk.Button(ventana, text="Inicio", font=("Arial", 14), bg="#90EE90", fg="black", padx=20, pady=10, command=abrir_ventana_principal)
 boton_inicio.pack(pady=20)
 
